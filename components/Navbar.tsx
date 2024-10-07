7857d58d4ae5d1c4fcc0a0993911ca5a0aefafff
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-10-07 06:25:00 +0100

    update: created nav component

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center max-w-[1400px] mx-auto px-3 md:px-6 py-4 md:py-6 border-b shadow-sm">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
