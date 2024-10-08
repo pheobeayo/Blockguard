7d20f4eb928a73b202ef01dae0d2de23e5501950
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-10-08 17:36:35 +0100

    update: Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -79,7 +79,7 @@ const Hero = () => {
             Why Choose Us
           </h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-4 w-full pt-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-4 w-full pt-6">
           {wcus.map((item) => (
             <div
               key={item.title}
